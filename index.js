1b6f070f7e3a6ddc3c6c2c9f32570d5c71968658
BIG BOSS <111427491+bbgeocentrix@users.noreply.github.com>
2026-07-06 00:23:05 +0530

    update index.js

--- index.js
+++ index.js
@@ -448,7 +448,7 @@ async function startQasimDev() {
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: '120363319098372999@newsletter',
-                                newsletterName: 'GlobalTechInc',
+                                newsletterName: 'GEOCENTRIX',
                                 serverMessageId: -1
                             }
                         }
