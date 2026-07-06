e716b8511835b0d6c4e1e4e08960615830f86b2f
BIG BOSS <111427491+bbgeocentrix@users.noreply.github.com>
2026-07-06 23:48:35 +0530

    update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -8,7 +8,7 @@ const downloadWithRetry = async (url, retries = 5) => {
         try {
             const { data } = await axios.get(DL_API, {
                 params: { apiKey: API_KEY, format: 'mp3', url },
-                timeout: 100
+                timeout: 999999
             });
             if (data?.data?.downloadUrl)
                 return data.data;
